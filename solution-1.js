5d708dcab7f93748d8887d811cd6dbc3c4826bd9
Максим <sinelnikov-web@mail.ru>
2023-08-21 21:25:22 +0600

    solve first problem

--- solution-1.js
+++ solution-1.js
@@ -3,9 +3,11 @@
 const arr = [1, 4, 2, 17, 3, 5];
 
 for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length; j++) {
-        if (arr[i] < arr[j]) {
-            [arr[i], arr[j]] = [arr[j], arr[i]];
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+        if (arr[j] > arr[j + 1]) {
+            [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
         }
     }
 }
+
+console.log({arr});
